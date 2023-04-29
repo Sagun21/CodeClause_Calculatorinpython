e86d780f69aa4ff2c48a76fff5f985111c65ed66
Sagun21 <129666761+Sagun21@users.noreply.github.com>
2023-04-30 00:10:44 +0530

    Add files via upload

---
+++ Calculator In Python.py
@@ -0,0 +1,16 @@
+number_1 = int(input('Please, Enter the first number: '))
+number_2 = int(input('Please, Enter the second number: '))
+ 
+
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+ 
+
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+ 
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+ 
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
